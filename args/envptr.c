d16b68e0b70066a39388f8e619243dada0a9a58e
root <root@kali.moo.com>
2022-11-11 20:46:05 +0700

    add first trial on env vars by pointer to pointer

---
+++ args/envptr.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+
+/*This program is to test the argv and env variables
+ * while argv is using single pointer the env in its declaration here 
+ * uses pointer to pointer.*/
+
+int main (int argc, char *argv[], char **env_ptr) {
+	// we use env ptr since this is not an array but pointer to pointer
+	
+	int i = 0;
+
+	// I think the devs put i here as declaration must be on top of the function
+	
+	printf("\ncommand line arg count : %d", argc);
+
+	printf("\nexecutable file: %s", argv[0]);
+
+	// for the rest of argv if exist:
+	if (argc > 1) {
+		for (i = 1; i < argc ; i++) {
+			printf("\narg %d : %s", i, argv[i]);
+		}
+	}
+
+	// now for the environmet variable
+	i = 0;
+	while (*env_ptr != NULL) {
+		i++;
+		printf("\nenv var %d : %s", i, *(env_ptr++));
+	}
+
+	printf("\n");
+	return 0;
+}
