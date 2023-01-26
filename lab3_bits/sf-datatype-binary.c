e8d9179d17bd0c9bbb4e004e9c64c87d76803519
m00racle <yanuar.prakosa@gmail.com>
2023-01-26 07:33:38 +0700

    add lab 3 Bytes and Bits

---
+++ lab3_bits/sf-datatype-binary.c
@@ -0,0 +1,18 @@
+/*This is lab 3: 
+ * Byte and Bits 
+ *
+ * How Bynary data type stored in memory
+ * */
+
+#include <stdio.h>
+
+int main()
+{
+	char c;
+	int i;
+
+	// print c in decimal int placehoder
+	printf("c = 255, displayed value c = %d\n", c = 255);
+
+	return 0;
+}
