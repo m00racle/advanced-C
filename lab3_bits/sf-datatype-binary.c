584df54d519e23b4ad3a54cebd9642bdf51ebf2c
m00racle <yanuar.prakosa@gmail.com>
2023-01-26 07:51:47 +0700

    finish lab 3 Byte and bits see result.txt
    For more information about lab 3 see review of the notepad
    for Byte and Bits section of the course.
    
    All reasoning are there including defaule argument promotion

--- lab3_bits/sf-datatype-binary.c
+++ lab3_bits/sf-datatype-binary.c
@@ -15,6 +15,11 @@ int main()
 	printf("c = 255, displayed value c = %d\n", c = 255);
 	printf("c = 128, displayed value c = %d\n", c = 128);
 	printf("c = -1 , displayed value c = %x\n", c = -1);
+	printf("c = 256, displayed value c = %d\n", c = 256);
+
+	printf("i = -2 , displayed value i = %x\n", i = -2);
+	printf("i = 256, displayed value i = %x\n", i = 256);
+	printf("i =-256, displayed value i = %x\n", i = -256);
 
 	return 0;
 }
