25ba36fca2fee34ca8a6b5848b891300ee4b3a9f
m00racle <yanuar.prakosa@gmail.com>
2023-02-05 14:40:01 +0700

    add conditional block if c > 'c' trial

--- lab5_pitfall_types/char_minmax_pitfall.c
+++ lab5_pitfall_types/char_minmax_pitfall.c
@@ -4,6 +4,10 @@ int main() {
 	char c1 = 'a', c2 = 'b', c;
 	// I declare 3 char type objects and only define two of them
 	c = c1 + c2;
+	if (c > 'c') 
+		printf("True\n");
+	else
+		printf("False\n");
 
 	return 0;	
 }
