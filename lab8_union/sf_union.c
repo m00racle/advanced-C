040124304bdab9e39c81056ac5b2133fea4d580b
m00racle <yanuar.prakosa@gmail.com>
2023-03-13 12:17:28 +0700

    ANSWER: union shared memory deep look

--- lab8_union/sf_union.c
+++ lab8_union/sf_union.c
@@ -3,13 +3,35 @@
 int main() {
 	union sf_un {
 		short val;
-		char ch;
+		char ch[2];
 	};
 
 	union sf_un u;
+	int i;
 
 	u.val = 258;
-	u.ch = 1;
+		
+
 	printf("the union val: %d\n", u.val);
-	printf("the union ch : %d\n", u.ch);
+	printf("the union ch : %d\n", u.ch[0]);
+
+	printf("start of the u.val address: %p\n", &u.val);
+	
+	
+	for (i=0; i<2; i++) {
+		printf("address %p value: 0x%x\n", &u.ch[i], (unsigned char)(u.ch[i]));
+	}
+	
+	// test put value to u.ch
+	u.ch[0] = 1;
+	
+	printf("the union val: %d\n", u.val);
+	printf("the union ch : %d\n", u.ch[0]);
+
+	printf("start of the u.val address: %p\n", &u.val);
+	
+	for (i=0; i<2; i++) {
+		printf("address %p value: 0x%x\n", &u.ch[i], (unsigned char)(u.ch[i]));
+	}
+	
 }
