c1f6117fbe525a408f7747ef46d90907c96a2b8a
m00racle <33504187+m00racle@users.noreply.github.com>
2023-05-17 08:45:39 +0700

    first trial bit union

--- lab9_bit_fields/sf_bitfunion.c
+++ lab9_bit_fields/sf_bitfunion.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 // I want to make bitfield inside a union
 union unif {
-	
+	unsigned char c1:3;
+    unsigned short s1:2;
 };
+
+int main() {
+    union unif uno;
+    uno.s1 = 3;
+    printf("address of the  uno: %p\n", &uno);
+    printf("address of the s1: %p\n", uno.s1);
+    printf("hex value of the s1: %x\n", uno.s1);
+    printf("hex value of the uno: %x\n", uno);
+    printf("decimal value of the s1: %d\n", uno.s1);
+    printf("decimal value of the uno: %d\n", uno);
+}
