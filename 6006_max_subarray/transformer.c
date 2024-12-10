6ba395ae70bc67eaff4d840665465cb635312ac4
mooracle <yanuar.prakosa@gmail.com>
2024-12-10 14:54:27 +0700

    find max sub array using divide and conquer but with bug
    base case DivMaxSubarray
    
    simplify by making some code inline seperated by ;
    
    add helper func copyArray
    
    no runtime error but buggy results

--- 6006_max_subarray/transformer.c
+++ 6006_max_subarray/transformer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <math.h>
 
 // declaring functions
 
@@ -11,6 +12,30 @@ int* FindMaxSubarray(int* A, int low, int high);
 
 // declaring max-crossing-subarray
 int* FindMaxCrossingSubarray(int *A, int low, int mid, int high);
+
+// declaring DivMaxSubarray
+int* DivMaxSubarray(int *A, int low, int high);
+
+// helper function to copy one array to another:
+void copyArray(int *src, int *dest, int size) {
+    /*
+        helper function to copy one array to another.
+        Required if you want to preserve a state from returned static array
+        the static array will be changed for each call to its function thus the previous state will also changed
+        even as you refer it using new variable, this variable will only be a pointer to the same address as the static array
+        thus when the static array elements changed then the varible element also changed thus unable to preserve the state
+
+        Parameters:
+            - src : pointer in this case for array consist of int(s) act as source of copy
+            - dest : pointer to another prepared array for int(s) act as destination of copy
+            - size : int specified number of element NOT the total size in bytes!!!
+    */
+  // Loop through each element of the arrays
+  for (int i = 0; i < size; i++) {
+    // Copy the i-th element from source to destination
+    dest[i] = src[i];
+  }
+}
 
 void test_run(int *A, int length, char* test_msg)
 {
@@ -23,7 +48,7 @@ void test_run(int *A, int length, char* test_msg)
     */
 
     // select the method to find maximum sub-array
-    int *results = FindMaxSubarray(A, 0, length - 1);
+    int *results = DivMaxSubarray(A, 0, length - 1);
     
     // print out the results
     printf("\nTest Run %s results: ", test_msg);
@@ -47,6 +72,70 @@ int main()
     return 0;
 }
 
+int* DivMaxSubarray(int *A, int low, int high)
+{
+    /*  
+        using divide and conquer recursive algorithm
+        find max sub array
+
+        Parameters:
+        A (int pointer Array) = array of int to find max sub array
+        low (int) = lowest index of array element in scope
+        high (int) = highest index of array element in scope
+    */
+    // I don't think I need to reset the returned array (div_data) 
+    // the contains in it will not be used as if comparison
+    static int div_data[3];
+    // declare arrays to store low, high indexes, and max sum of each merge part
+    int left_data[3];
+    int right_data[3];
+    int cross_data[3];
+
+    int mid; // this is the complementary index between low and high
+    // base case:
+    if (high == low)
+    {
+        // means there only one element and thus the max sum will be that element only
+        // return {low, high, the only element in subarray}
+        div_data[0] = low;
+        div_data[1] = high;
+        div_data[2] = A[low];
+
+        return div_data;
+    } else
+    {
+        // run the recursive dividing steps by half of the arrray length
+        mid = floor((low + high) / 2);
+
+        // run the merge steps by selecting most optimum sum
+        // begin with the left side low to mid:
+        copyArray(DivMaxSubarray(A, low, mid), left_data, 3);
+        // right side mid+1 to high
+        copyArray(DivMaxSubarray(A, mid + 1, high), right_data, 3);
+        // cross test 
+        copyArray(FindMaxCrossingSubarray(A, low, mid, high), cross_data, 3);
+
+        // determine which is the left, right, or cross to be returned to below recursive step;
+        // REMEBER ALWAYS RETURN ARRAY AS div_data since it is the static array here!
+        // meaning you need to copy the selected array to div_data array first
+        // note the sum is in each data index [2]
+        if (left_data[2] >= right_data[2] && left_data[5] >= cross_data[2])
+        {
+            copyArray(left_data, div_data, 3);
+        } 
+        else if (right_data[2] > left_data[2] && right_data[5] > cross_data[2])
+        {
+            copyArray(right_data, div_data, 3);
+        }
+        else
+        {
+            copyArray(cross_data, div_data, 3);
+        }
+
+        return div_data;
+    }
+}
+
 // define FindMaxSubarray(int* A, int low, int high)
 int* FindMaxSubarray(int* A, int low, int high)
 {
@@ -63,10 +152,7 @@ int* FindMaxSubarray(int* A, int low, int high)
     static int max_data[3];
     // reset back to zero since max_data is static to ensure clean start for multiple calls.
     // note you can't just use max_data = {0, 0, 0}!
-    for (int i = 0; i < 3; i++)
-    {
-        max_data[i] = 0;
-    }
+    for (int i = 0; i < 3; i++) max_data[i] = 0;
 
     // initialize other variables:
     int sum = 0;
@@ -93,9 +179,7 @@ int* FindMaxSubarray(int* A, int low, int high)
             if (max_sum > max_data[2])
             {
                 // put the data into max_data
-                max_data[0] = start;
-                max_data[1] = end;
-                max_data[2] = max_sum;
+                max_data[0] = start; max_data[1] = end; max_data[2] = max_sum; 
             }
 
             // because it is new low take it as new start
@@ -109,9 +193,7 @@ int* FindMaxSubarray(int* A, int low, int high)
     if (max_sum > max_data[2])
     {
         // put the data into max_data
-        max_data[0] = start;
-        max_data[1] = end;
-        max_data[2] = max_sum;
+        max_data[0] = start; max_data[1] = end; max_data[2] = max_sum;
     }
 
     return max_data;
@@ -133,6 +215,8 @@ int* FindMaxCrossingSubarray(int *A, int low, int mid, int high)
     int sum = 0;
     int max_left;
     int max_right;
+    // preparing the array to be returned : NOTE: it must be static to survive the end of scope
+    static int result[3];
     
     //decrement the index in array A from mid to low:
     for (int i = mid; i >= low; i--)
@@ -164,13 +248,9 @@ int* FindMaxCrossingSubarray(int *A, int low, int mid, int high)
         }
     }
 
-    // preparing the array to be returned : NOTE: it must be static to survive the end of scope
-    static int result[3];
     // You can't directly initialize result[3] = {max_left, max_right, left_sum + right_sum}
     // because they are not constants, thus you need to assign them later 
-    result[0] = max_left;
-    result[1] = max_right;
-    result[2] = left_sum + right_sum;
+    result[0] = max_left; result[1] = max_right; result[2] = left_sum + right_sum;
 
     return result;
 }
