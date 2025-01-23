9821d53fbc3439bb47af43a05a42ee6485d74a65
mooracle <yanuar.prakosa@gmail.com>
2025-01-23 16:03:13 +0700

    add experiment data:

--- lab3_bits/sf-datatype-binary.c
+++ lab3_bits/sf-datatype-binary.c
@@ -13,24 +13,30 @@ int main()
 
 	// print c in decimal int placehoder
 	printf("c = 255, displayed value c = %d\n", c = 255);
-	printf("c = 255, hex value c = %x\n", c);
+	printf("c = 255, hex value c = 0x%x\n", c);
 	printf("c = 511, display value c = %d\n", c = 511);
-	printf("c = 511, hex value c = %x\n", c);
-	printf("c = -1 , hex value c = %x\n", c = -1);
+	printf("c = 511, hex value c = 0x%x\n", c);
+	printf("c = -1 , hex value c = 0x%x\n", c = -1);
 	printf("c = 128, displayed value c = %d\n", c = 128);
-	printf("c = 128, hex value c = %x\n", c);
+	printf("c = 128, hex value c = 0x%x\n", c);
 	printf("c = 384, displayed value c = %d\n", c = 384);
-	printf("c = 384, hex value c = %x\n", c);
-	printf("c = -128, hex value c = %x\n", c = -128);
+	printf("c = 384, hex value c = 0x%x\n", c);
+	printf("c = -128, hex value c = 0x%x\n", c = -128);
 	printf("c = 256, displayed value c = %d\n", c = 256);
-	printf("c = 256, hex value c = %x\n", c);
+	printf("c = 256, hex value c = 0x%x\n", c);
 	printf("c = 512, display value c = %d\n", c = 512);
-	printf("c = 512, hex value c = %x\n", c);
-	printf("c = 0, hex value c = %x\n", c = 0);
+	printf("c = 512, hex value c = 0x%x\n", c);
+	printf("c = 0, hex value c = 0x%x\n", c = 0);
+	printf("c = 341, display value c = %d\n", c = 341);
+	printf("c = 341, hex value c = 0x%x\n", c);
+	printf("c = 170, display value c = %d\n", c = 170);
+	printf("c = 170, hex value c = 0x%x\n", c);
+	printf("c = 682, display value c = %d\n", c = 682);
+	printf("c = 682, hex value c = 0x%x\n", c);
 
-	printf("i = -2 , displayed value i = %x\n", i = -2);
-	printf("i = 256, displayed value i = %x\n", i = 256);
-	printf("i =-256, displayed value i = %x\n", i = -256);
+	printf("i = -2 , displayed value i = 0x%x\n", i = -2);
+	printf("i = 256, displayed value i = 0x%x\n", i = 256);
+	printf("i =-256, displayed value i = 0x%x\n", i = -256);
 
 	return 0;
 }
